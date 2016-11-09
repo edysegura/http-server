4f339f263fd1cd706c17d7dab3195d50cedd3032
Edy Segura <ednardo@inatel.br>
2016-11-08 23:21:41 -0200

    Implemented data listener in server.js

--- server.js
+++ server.js
@@ -9,12 +9,25 @@ function start(route, handler) {
     http
         .createServer(onRequest)
         .listen(port, onStart)
-    
+
     function onRequest(request, response) {
         console.log("Request for: " + request.url)
-        
+        console.log("HTTP Method: " + request.method)
+
         let pathname = url.parse(request.url).pathname
-        route(pathname, handler, response)
+        let dataChunks = []
+
+        request.setEncoding("utf8")
+        
+        request.addListener("data", function onReceive(dataChunk) {
+            console.log("Data chunck: " + dataChunk)
+            dataChunks.push(dataChunk)
+        })
+
+        request.addListener("end", function onEnd() {
+            let data = dataChunks.join("")
+            route(pathname, handler, response, data)
+        })
     }
 }
 
